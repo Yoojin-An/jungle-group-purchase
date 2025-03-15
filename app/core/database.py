6173f753387c70e765370b26bc636e62b6ddef57
안유진 <yoojin.an0@gmail.com>
2025-03-16 03:09:19 +0900

    modify: core

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 
-def init_db():
-    client = MongoClient('localhost', 27017)
-    db = client.boards
-    users_collection = db.users
-    tokens_collection = db.tokens
+class Database:
+    def __init__(self):
+        client = MongoClient('localhost', 27017)
+        self.collection = client.board
