14133c22ea04c73e7c8d225abb9bea2ab0add07a
안유진 <yoojin.an0@gmail.com>
2025-03-16 03:12:53 +0900

    modify: run.py
    - 모듈 import 시에도 CORS, Scheduler 정상 작동하도록 실행 위치를 __main__ 밖으로 이동

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from app import create_app
 from flask_cors import CORS
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = create_app()
+CORS(app, supports_credentials=True)
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.start()
 
 if __name__ == '__main__':
-    CORS(app, supports_credentials=True)
-    scheduler = BackgroundScheduler(daemon=True)
-    scheduler.start()
     app.run('0.0.0.0', port=5001, debug=True)
